a854a2ef036728305587cd377723a8543333f159
Andrés Torres <andresto1991@gmail.com>
2016-07-12 23:16:03 -0300

    Local IP package for testing on mobile devices. Devtools disabled

--- webpack/dev.config.js
+++ webpack/dev.config.js
@@ -5,7 +5,7 @@ var fs = require('fs');
 var path = require('path');
 var webpack = require('webpack');
 var assetsPath = path.resolve(__dirname, '../static/dist');
-var host = (process.env.HOST || 'localhost');
+var host = (process.env.HOST || require('my-local-ip')());
 var port = (+process.env.PORT + 1) || 3001;
 
 var bourbonIncludePaths = require('node-bourbon').includePaths;
