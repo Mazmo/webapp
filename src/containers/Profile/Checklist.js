d0daee793548196c8194dac6f2e3b70aed743447
Andrés Torres <andresto1991@gmail.com>
2016-11-08 02:30:16 -0300

    Checklist fully functional

--- src/containers/Profile/Checklist.js
+++ src/containers/Profile/Checklist.js
@@ -1,5 +1,6 @@
 import React, { Component, PropTypes } from 'react';
 import { asyncConnect } from 'redux-async-connect';
+import classNames from 'classnames';
 
 @asyncConnect([
   {
@@ -17,14 +18,46 @@ export default class Checklist extends Component {
 
   render() {
     const styles = require('./Checklist.scss');
-    if (this.props.checklist.error) {
-      return <div>ERROR: {this.props.checklist.error}</div>;
+    const error = this.props.checklist.error;
+    const checklist = this.props.checklist.results;
+
+    if (error) {
+      return <div className={styles.error}>{error}</div>;
     }
 
-    return (
+    return checklist ? (
       <div className={styles.checklist}>
-        <pre>{JSON.stringify(this.props.checklist.results, null, 2)}</pre>
+
+        {checklist.map(group => {
+          const items = group.items.filter(item => item.selection);
+
+          if (items.length) {
+            return (
+              <div className={styles.group} key={group.category.id}>
+                <div className={styles.header}>
+                  <h3 className={styles.headerTitle}>{group.category.name}</h3>
+                  <span className={styles.headerExperience}>Experience</span>
+                  <span className={styles.headerInterest}>Interest</span>
+                </div>
+                {items.map(item => {
+                  const row = item.selection;
+                  const expClasses = classNames(styles.rowExperience, styles[`level${row.experience}`]);
+                  const intClasses = classNames(styles.rowInterest, styles[`level${row.interest}`]);
+
+                  return (
+                    <div className={styles.row} key={item.id}>
+                      <span className={styles.rowLabel}>{item.name}</span>
+                      <span className={expClasses}>{row.experience_text}</span>
+                      <span className={intClasses}>{row.interest_text}</span>
+                    </div>
+                  );
+                })}
+              </div>
+            );
+          }
+        })}
+
       </div>
-    );
+    ) : null;
   }
 }
