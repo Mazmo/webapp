13b341b810c130ab818b038476aa9302f9d28be9
Andrés Torres <andresto1991@gmail.com>
2016-11-08 02:55:44 -0300

    Fixed redux interactions with profile publications

--- src/containers/Profile/Publications.js
+++ src/containers/Profile/Publications.js
@@ -2,6 +2,12 @@ import React, { Component, PropTypes } from 'react';
 import { connect } from 'react-redux';
 import { asyncConnect } from 'redux-async-connect';
 import { Publication } from '../../components';
+import {
+  createComment,
+  reactToPublication,
+  reactToComment,
+  loadReactions
+} from 'redux/modules/publications';
 
 @asyncConnect([
   {
@@ -12,14 +18,22 @@ import { Publication } from '../../components';
 ])
 @connect(
   state => ({
-    me: state.auth.user
-  })
+    me: state.auth.user,
+    canCreateComment: !state.publications.creatingComment,
+    publications: state.publications.data
+  }), { createComment, reactToPublication, reactToComment, loadReactions}
 )
 export default class Publications extends Component {
   static propTypes = {
     me: PropTypes.object,
+    publications: PropTypes.array.isRequired,
     profile: PropTypes.object.isRequired,
-    data: PropTypes.array.isRequired
+    data: PropTypes.array.isRequired,
+    createComment: PropTypes.func.isRequired,
+    canCreateComment: PropTypes.bool.isRequired,
+    reactToPublication: PropTypes.func.isRequired,
+    reactToComment: PropTypes.func.isRequired,
+    loadReactions: PropTypes.func.isRequired
   };
 
   render() {
@@ -32,6 +46,11 @@ export default class Publications extends Component {
               key={i}
               me={this.props.me}
               data={publication}
+              react={this.props.reactToPublication}
+              createComment={this.props.createComment}
+              canCreateComment={this.props.canCreateComment}
+              reactToComment={this.props.reactToComment}
+              loadReactions={this.props.loadReactions}
             />
           );
         })}
